65d1ebe5e9e626f493317c10e8adb4ab7610d553
WilsonVinson <cprewilsonvinson@gmail.com>
2024-01-09 16:06:23 +0800

    main

---
+++ project/resource/item_data.gd
@@ -0,0 +1,11 @@
+@tool
+extends Resource
+class_name ItemData
+
+@export var id : int # ID
+@export var name : String # 名称
+@export var durability : int # 耐久
+@export var pile_max : int # 堆叠
+@export var num : int # 数量
+
+@export var icon : Texture
