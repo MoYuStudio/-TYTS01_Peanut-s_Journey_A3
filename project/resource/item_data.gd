2241f91bc24ab5dfca82e0d4cf3467c859a20676
WilsonVinson <cprewilsonvinson@gmail.com>
2024-01-15 17:52:59 +0800

    main

--- project/resource/item_data.gd
+++ project/resource/item_data.gd
@@ -3,10 +3,10 @@
 extends Resource
 class_name Self_ItemData
 
-@export var id : int         	# ID
-@export var name : String    	# 名称
-@export var durability : int 	# 耐久
-@export var max_pile : int   	# 堆叠
-@export var num : int        	# 数量
+@export var id : int           	# ID
+@export var name : String      	# 名称
+@export var durability : float 	# 耐久
+@export var max_pile : int     	# 堆叠
+@export var num : int          	# 数量
 
 @export var icon : Texture
--- project/resource/tile_data.gd
+++ project/resource/tile_data.gd
@@ -5,7 +5,7 @@ class_name Self_TileData
 
 @export var id : int         	# ID
 @export var name : String    	# 名称
-@export var hardness : int   	# 硬度
+@export var hardness : float   	# 硬度
 @export var num : int        	# 数量
 
 @export var icon : Texture
